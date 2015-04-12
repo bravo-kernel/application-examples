4f4d248c921035d2620300d26bb8a77c7489b095
bravo-kernel <bravo-kernel@users.noreply.github.com>
2015-04-12 23:34:07 +0200

    Adds more cocktails

--- blog-how-to-use-a-cakephp3-rest-api/config/Migrations/20150411161731_initial.php
+++ blog-how-to-use-a-cakephp3-rest-api/config/Migrations/20150411161731_initial.php
@@ -40,8 +40,18 @@ class Initial extends AbstractMigration
             "('7', 'Bloody Mary', 'Vodka based', '2015-04-11 09:52:02', null)," .
             "('8', 'Black Velvet', 'Beer based', '2015-04-11 09:52:02', null)," .
             "('9', 'Martini', 'Gin based', '2015-04-11 09:52:02', null)," .
-            "('10', 'Manhattan', 'Whisky based', '2015-04-11 09:52:03', null)," .
-            "('11', 'Beach Runner', 'Gin based', '2015-04-11 09:52:03', null)"
+            "('10', 'Manhattan', 'Whiskey based', '2015-04-11 09:52:03', null)," .
+            "('11', 'Bronx', 'Gin based', '2015-04-11 09:52:03', null)," .
+            "('12', 'Rose', 'Gin bassed', '2015-04-11 09:52:03', null)," .
+            "('13', 'Hot Shot', 'Coffee based', '2015-04-11 09:52:03', null)," .
+            "('14', 'Parrot', 'Tequila based', '2015-04-11 09:52:03', null)," .
+            "('15', 'Boxer', 'Whiskey based', '2015-04-11 09:52:03', null)," .
+            "('16', 'Monkey', 'Cointreau based', '2015-04-11 09:52:03', null)," .
+            "('17', 'Pink Panther', 'Cointreau based', '2015-04-11 09:52:03', null)," .
+            "('18', 'Zombie', 'Rum based', '2015-04-11 09:52:03', null)," .
+            "('19', 'Matador', 'Tequila based', '2015-04-11 09:52:03', null)," .
+            "('20', 'B52', 'Cream based', '2015-04-11 09:52:03', null)," .
+            "('21', 'Beach Runner', 'Gin based', '2015-04-11 09:52:03', null)"
         );
     }
 
