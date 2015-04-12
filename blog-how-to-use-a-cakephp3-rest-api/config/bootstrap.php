ec52c5d0a6cd26a83f1dcfc65559cdee055816e1
bravo-kernel <bravo-kernel@users.noreply.github.com>
2015-04-12 23:56:45 +0200

    Fixes bootstrap

--- blog-how-to-use-a-cakephp3-rest-api/config/bootstrap.php
+++ blog-how-to-use-a-cakephp3-rest-api/config/bootstrap.php
@@ -180,7 +180,6 @@ Request::addDetector('tablet', function ($request) {
  */
 
 Plugin::load('Migrations');
-Plugin::load('Crud');
 
 // Only try to load DebugKit in development mode
 // Debug Kit should not be installed on a production system
@@ -194,3 +193,5 @@ if (Configure::read('debug')) {
 DispatcherFactory::add('Asset');
 DispatcherFactory::add('Routing');
 DispatcherFactory::add('ControllerFactory');
+
+Plugin::load('Crud');
