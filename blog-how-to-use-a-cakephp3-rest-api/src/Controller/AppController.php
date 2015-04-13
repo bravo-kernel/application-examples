6baf1a6d4868864527f43033e46eea67711f89e6
bravo-kernel <bravo-kernel@users.noreply.github.com>
2015-04-13 14:37:50 +0200

    Moves paginator settings to CocktailsController

--- blog-how-to-use-a-cakephp3-rest-api/src/Controller/AppController.php
+++ blog-how-to-use-a-cakephp3-rest-api/src/Controller/AppController.php
@@ -1,38 +1,23 @@
 <?php
 namespace App\Controller;
 
-use Cake\Controller\Controller;
+use App\Controller\AppController;
 
-class AppController extends Controller {
-
-    use \Crud\Controller\ControllerTrait;
-
-    public $components = [
-        'RequestHandler',
-        'Crud.Crud' => [
-            'actions' => [
-                'Crud.Index',
-                'Crud.View',
-                'Crud.Add',
-                'Crud.Edit',
-                'Crud.Delete'
-            ],
-            'listeners' => [
-                'Crud.Api',
-                'Crud.ApiPagination',
-                'Crud.ApiQueryLog'
-            ]
+class CocktailsController extends AppController
+{
+    public $paginate = [
+        'page' => 1,
+        'limit' => 10,
+        'maxLimit' => 100,
+        'fields' => [
+            'id',
+            'name',
+            'description'
         ],
-        'Paginator' => [
-            'page' => 1,
-            'limit' => 10,
-            'maxLimit' => 100,
-            'fields' => [
-                'id', 'name', 'description'
-            ],
-            'sortWhitelist' => [
-                'id', 'name', 'description'
-            ]
+        'sortWhitelist' => [
+            'id',
+            'name',
+            'description'
         ]
     ];
 }
--- blog-how-to-use-a-cakephp3-rest-api/src/Controller/CocktailsController.php
+++ blog-how-to-use-a-cakephp3-rest-api/src/Controller/CocktailsController.php
@@ -5,5 +5,15 @@ use App\Controller\AppController;
 
 class CocktailsController extends AppController
 {
-
+    public $paginate = [
+        'page' => 1,
+        'limit' => 10,
+        'maxLimit' => 100,
+        'fields' => [
+            'id', 'name', 'description'
+        ],
+        'sortWhitelist' => [
+            'id', 'name', 'description'
+        ]
+    ];
 }
