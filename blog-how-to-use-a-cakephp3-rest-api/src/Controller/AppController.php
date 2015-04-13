e6adae86bbd67d9b9acc462432a9adadc6f4ac23
bravo-kernel <bravo-kernel@users.noreply.github.com>
2015-04-13 21:48:37 +0200

    You donkey

--- blog-how-to-use-a-cakephp3-rest-api/src/Controller/AppController.php
+++ blog-how-to-use-a-cakephp3-rest-api/src/Controller/AppController.php
@@ -1,23 +1,27 @@
 <?php
 namespace App\Controller;
 
-use App\Controller\AppController;
+use Cake\Controller\Controller;
 
-class CocktailsController extends AppController
-{
-    public $paginate = [
-        'page' => 1,
-        'limit' => 10,
-        'maxLimit' => 100,
-        'fields' => [
-            'id',
-            'name',
-            'description'
-        ],
-        'sortWhitelist' => [
-            'id',
-            'name',
-            'description'
+class AppController extends Controller {
+
+    use \Crud\Controller\ControllerTrait;
+
+    public $components = [
+        'RequestHandler',
+        'Crud.Crud' => [
+            'actions' => [
+                'Crud.Index',
+                'Crud.View',
+                'Crud.Add',
+                'Crud.Edit',
+                'Crud.Delete'
+            ],
+            'listeners' => [
+                'Crud.Api',
+                'Crud.ApiPagination',
+                'Crud.ApiQueryLog'
+            ]
         ]
     ];
 }
