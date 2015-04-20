e3b3d68531cfa6eb470fea0862ac7fd374a0781b
bravo-kernel <bravo-kernel@users.noreply.github.com>
2015-04-20 09:06:58 +0000

    Adds jwt blog

---
+++ blog-how-to-add-jwt-authentication-to-a-cakephp-3-rest-api/README.md
@@ -0,0 +1,3 @@
+# Blog sources
+
+## [How to prefix route a CakePHP 3 REST API](http://www.bravo-kernel.com/2015/04/how-to-prefix-route-a-cakephp-3-rest-api/)
---
+++ blog-how-to-add-jwt-authentication-to-a-cakephp-3-rest-api/src/Model/Table/CocktailsTable.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Model\Table;
+
+use App\Model\Entity\Cocktail;
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Cocktails Model
+ */
+class CocktailsTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        $this->table('cocktails');
+        $this->displayField('name');
+        $this->primaryKey('id');
+        $this->addBehavior('Timestamp');
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->add('id', 'valid', ['rule' => 'numeric'])
+            ->allowEmpty('id', 'create')
+            ->requirePresence('name', 'create')
+            ->notEmpty('name')
+            ->requirePresence('description', 'create')
+            ->notEmpty('description');
+
+        return $validator;
+    }
+}
