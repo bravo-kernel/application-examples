54fb75f01acc357c1c4c0a5795e9e843e5b934cb
bravo-kernel <bravo-kernel@users.noreply.github.com>
2015-04-21 16:09:52 +0000

    Rewrite due to sub-namespaced AppController

--- blog-how-to-prefix-route-a-cakephp3-rest-api/src/Controller/Api/AppController.php
+++ blog-how-to-prefix-route-a-cakephp3-rest-api/src/Controller/Api/AppController.php
@@ -1,13 +1,15 @@
 <?php
-namespace App\Controller;
+namespace App\Controller\Api;
 
 use Cake\Controller\Controller;
+use Cake\Event\Event;
 
-class AppController extends Controller {
-
+class AppController extends Controller
+{
     use \Crud\Controller\ControllerTrait;
 
     public $components = [
+        'RequestHandler',
         'Crud.Crud' => [
             'actions' => [
                 'Crud.Index',
@@ -15,6 +17,11 @@ class AppController extends Controller {
                 'Crud.Add',
                 'Crud.Edit',
                 'Crud.Delete'
+            ],
+            'listeners' => [
+                'Crud.Api',
+                'Crud.ApiPagination',
+                'Crud.ApiQueryLog'
             ]
         ]
     ];
--- blog-how-to-prefix-route-a-cakephp3-rest-api/src/Controller/CocktailsController.php
+++ blog-how-to-prefix-route-a-cakephp3-rest-api/src/Controller/CocktailsController.php
@@ -1,7 +1,7 @@
 <?php
-namespace App\Controller;
+namespace App\Controller\Api;
 
-use App\Controller\AppController;
+use App\Controller\Api\AppController;
 
 class CocktailsController extends AppController
 {
