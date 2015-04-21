a224a7f1d4af90dd581cf0c4ed0994a59f5cdb97
bravo-kernel <bravo-kernel@users.noreply.github.com>
2015-04-21 16:20:06 +0000

    Fix controller mixup

--- blog-how-to-prefix-route-a-cakephp3-rest-api/src/Controller/CocktailsController.php
+++ blog-how-to-prefix-route-a-cakephp3-rest-api/src/Controller/CocktailsController.php
@@ -1,7 +1,7 @@
 <?php
-namespace App\Controller\Api;
+namespace App\Controller;
 
-use App\Controller\Api\AppController;
+use App\Controller\AppController;
 
 class CocktailsController extends AppController
 {
